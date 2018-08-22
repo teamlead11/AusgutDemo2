b016307fdc87dca0eb03c7c7e8dd953e99ea6076
Prabu_s <Prabu_s@CHD115158.photonint.com>
2018-08-22 19:51:21 +0530

    Reverse String Commit By Prabu

--- src/gitpack1/Framework.java
+++ src/gitpack1/Framework.java
@@ -88,4 +88,14 @@ public static void revBySilambarasan() {
 	System.out.println(rev);
 	
 }
+public static void reverseByPrabu(String name,String reverse) 
+{
+	for(int i=name.length()-1; i>=0; i--) {
+		char ch = name.charAt(i);
+	    reverse = reverse + ch;
+	}
+	System.out.println(reverse);
+	
+	
+}
 }
