2b221f932a9b4ebb76d63696bc6416903912b134
RajasriRajendran <postmail2rajasri@gmail.com>
2018-08-22 12:15:46 +0530

    Resolved code

--- src/gitpack1/Framework.java
+++ src/gitpack1/Framework.java
@@ -7,6 +7,7 @@ public class Framework {
 		System.out.println("this is my framework");
 		add(5, 6);
 		add(5, 6, 7);
+		reverseByRajasri();
 
 	}
 
@@ -16,5 +17,17 @@ public class Framework {
 
 	public static void add(int a, int b, int c) {
 		System.out.println(a + b + c);
+	}
+	public static void reverseByRajasri() {
+		String s = "Rajasri";
+		String s1 = "";
+			int len = s.length();
+			for (int i = len-1;i>0;i++)
+			{
+				char ch = s.charAt(i);
+				s1=s1+ch;
+			}
+			System.out.println(s1);
+	
 	}
 }
