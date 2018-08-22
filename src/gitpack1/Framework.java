f5150d38c0c54011c5d49168a86a47bb7a58ac9e
teamlead11 <automationlead11@gmail.com>
2018-08-22 10:46:36 +0530

    added add method

--- src/gitpack1/Framework.java
+++ src/gitpack1/Framework.java
@@ -5,6 +5,10 @@ public class Framework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my framework");
+		add(5, 6, 7);
 	}
 
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 }
