6ff401f2b12bacad9c16fbdba7fc2fb23e4ab760
GayatriKannan <gayu93.31@gmail.com>
2018-08-22 11:57:04 +0530

    Reverse number method add by me

--- src/gitpack1/Framework.java
+++ src/gitpack1/Framework.java
@@ -3,10 +3,10 @@ package gitpack1;
 public class Framework {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("this is my framework");
 		add(5, 6);
 		add(5, 6, 7);
+		revbyGayatri(a);
 
 	}
 
@@ -17,4 +17,17 @@ public class Framework {
 	public static void add(int a, int b, int c) {
 		System.out.println(a + b + c);
 	}
+	
+	
+	public static void revbyGayatri(int a) {
+		int i=0,j=0;
+		while(a>0) {
+			i = a%10;
+			j=(j*10)+i;
+			a=a/10;
+
+			}		return j;
+
+	}
+	
 }
