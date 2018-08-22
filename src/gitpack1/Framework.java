0b6eab8fbad234bb4275446fd25afae4d36eae5a
teamlead11 <automationlead11@gmail.com>
2018-08-22 10:25:54 +0530

    initial commit

---
+++ src/gitpack1/Framework.java
@@ -0,0 +1,10 @@
+package gitpack1;
+
+public class Framework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my framework");
+	}
+
+}
