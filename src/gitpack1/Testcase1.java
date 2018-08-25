3f884fc137809d745742bf44fb48633deb504b5b
vsanthi1982 <ss.machavolu@gmail.com>
2018-08-25 17:01:39 +0530

    Demoqa teststep executed succefully

--- src/gitpack1/Testcase1.java
+++ src/gitpack1/Testcase1.java
@@ -1,10 +1,12 @@
 package gitpack1;
 
+
 public class Testcase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test case1");
+		
 	}
 
 }
